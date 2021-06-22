758b39fb78550725a160e8d9094860f88f822210
JaagviCoding <81451397+JaagviCoding@users.noreply.github.com>
2021-06-21 20:42:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+block_image_width = 350;
+block_image_height =350;
+
+block_x = 10;
+block_y = 10;
+var player_object = "";
+
+function player_update(){
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object = Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+
+    });
+}
+
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object = Img;
+
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:block_y,
+            left:block_x
+        });
+        canvas.add(block_image_object);
+    });
+}
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e){
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed == '82')
+    {
+        new_image('rr1.png');
+        console.log("r");
+    }
+    if(keyPressed == '71') {
+        new_image('gr.png');
+        console.log("g");
+    }
+    if(keyPressed == '89') {
+       new_image('yr.png');
+        console.log("y");
+    }
+    if(keyPressed == '66') {
+        new_image('br.png');
+         console.log("b");
+}
+if(keyPressed == '80') {
+    new_image('pr.png');
+     console.log("p");
+}
+}
